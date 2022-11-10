fd5beb7e581c5ecfe9ded251461f3708aa31702a
Mohamed <mjibo671@hotmail.com>
2022-11-10 19:45:05 +0100

    first frontend select, from and where visualization.

--- IT-Project/src/app/app.component.ts
+++ IT-Project/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import  sqlQuery  from './sql_query.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'IT-Project';
+
+ sqlQueryData: any = sqlQuery;
+
+
 }
